024de2474911d9836f56ccb4be26b9b9d84a3c13
BRUNO <brunoovedoveto@gmail.com>
2016-05-08 23:48:55 -0300

    Começo de projeto. Função de inserir e buscar dados de arquivo

---
+++ main.c
@@ -0,0 +1,100 @@
+#include "stdio.h"
+
+typedef struct post{
+	char text[150];
+	char user[50];
+	char coordinates[20];
+	int like_count;
+	char language[30];
+	int share_count;
+	int views_count;
+} post;
+
+void printInterface(){
+	printf("|--------------------------|\n");
+	printf("|1- Inserir novo registro  |\n");
+	printf("|2- Listar registros       |\n");
+	printf("|--------------------------|\n");
+	printf("|0- Sair                   |\n");
+	printf("|--------------------------|\n");
+}
+
+void inserir(FILE* file){
+	post lpost;
+	printf("\n\n");
+	
+	fflush(stdin);
+	printf("Insira o texto do post\n");
+	gets(&lpost.text);	
+	
+	fflush(stdin);
+	printf("Insira o nome de usuario do post\n");
+	gets(&lpost.user);
+	
+	fflush(stdin);
+	printf("Insira as coordenadas do post\n");
+	gets(&lpost.coordinates);
+	
+	fflush(stdin);
+	printf("Insira a contagem de like do post\n");
+	scanf("%d", &lpost.like_count);
+	
+	fflush(stdin);
+	printf("Insira a linguagem do post\n");
+	gets(&lpost.language);
+	
+	fflush(stdin);
+	printf("Insira a contagem de compartilhamentos do post\n");
+	scanf("%d", &lpost.share_count);
+	
+	fflush(stdin);
+	printf("Insira a contagem de visualizacoes do post\n");
+	scanf("%d", &lpost.views_count);
+	
+	fwrite(&lpost, sizeof(post), 1, file);
+}
+
+void listar(FILE* file){
+	post lpost;
+	fseek(file, 0, 0);
+	
+	int recNo = 1;
+	while(fread(&lpost, sizeof(post), 1, file) > 0){
+		printf("***** Registro %d *****\n", recNo);
+		printf("Texto: %s\n", lpost.text);
+		printf("Usuario: %s\n", lpost.user);
+		printf("Coordenadas: %s\n", lpost.coordinates);
+		printf("Likes: %d\n", lpost.like_count);
+		printf("Linguagem: %s\n", lpost.language);
+		printf("Compartilhamentos: %d\n", lpost.share_count);
+		printf("Views: %d\n", lpost.views_count);
+		
+		printf("\n");
+		
+		recNo++;
+	}
+}
+
+int main(){
+	int op = 0;
+	
+	FILE* file = fopen("file.dat", "a+");
+	
+	printInterface();
+	scanf("%d", &op);
+	
+	switch(op){
+		case 1: inserir(file);
+		        break;
+		
+		case 2: listar(file);
+		        break;
+		        
+		case 0: exit(0);
+		        
+		default: printf("Opcao invalida!\n");
+	}	
+	
+	getch();
+	close(file);
+}
