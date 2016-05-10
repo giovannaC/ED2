49ab7377b5faf82a4f5b3282ab94a27953cff366
Giovanna <ginha_capires@hotmail.com>
2016-05-10 05:53:29 -0300

    Foi adicionado o começo do relatorio sobre o trabalho e identifiquei as funções de acordo com os itens propostos no trabalho

--- Projeto/myFile.h
+++ Projeto/myFile.h
@@ -13,42 +13,42 @@ typedef struct post{
 void inserir(FILE* file){
 	post lpost;
 	printf("\n\n");
-	
+
 	fflush(stdin);
 	printf("Insira o texto do post\n");
-	gets(&lpost.text);	
-	
+	gets(&lpost.text);
+
 	fflush(stdin);
 	printf("Insira o nome de usuario do post\n");
 	gets(&lpost.user);
-	
+
 	fflush(stdin);
 	printf("Insira as coordenadas do post\n");
 	gets(&lpost.coordinates);
-	
+
 	fflush(stdin);
 	printf("Insira a contagem de like do post\n");
 	scanf("%d", &lpost.like_count);
-	
+
 	fflush(stdin);
 	printf("Insira a linguagem do post\n");
 	gets(&lpost.language);
-	
+
 	fflush(stdin);
 	printf("Insira a contagem de compartilhamentos do post\n");
 	scanf("%d", &lpost.share_count);
-	
+
 	fflush(stdin);
 	printf("Insira a contagem de visualizacoes do post\n");
 	scanf("%d", &lpost.views_count);
-	
+
 	fwrite(&lpost, sizeof(post), 1, file);
 }
-
+// 3)
 void listar(FILE* file){
 	post lpost;
 	fseek(file, 0, 0);
-	
+
 	int RRN = 0;
 	while(fread(&lpost, sizeof(post), 1, file) > 0){
 		printf("***** Registro %d *****\n", RRN);
@@ -59,23 +59,23 @@ void listar(FILE* file){
 		printf("Linguagem: %s\n", lpost.language);
 		printf("Compartilhamentos: %d\n", lpost.share_count);
 		printf("Views: %d\n", lpost.views_count);
-		
+
 		printf("\n");
-		
+
 		RRN++;
 	}
 }
-
+// 5)
 void buscarUser(FILE* file){
 	post lpost;
 	char find[50];
-	
+
 	fseek(file, 0 , 0);
-	
+
 	fflush(stdin);
 	printf("\n\nInsira o usuario para buscar\n");
 	gets(&find);
-	
+
 	int achou = 0;
 	int RRN = 0;
 	while(fread(&lpost, sizeof(post), 1, file) > 0){
@@ -83,10 +83,10 @@ void buscarUser(FILE* file){
 		   achou = 1;
 		   break;
 	    }
-	    
+
 	    RRN++;
 	}
-	
+
 	if(achou){
 	    printf("\n***** Registro %d *****\n", RRN);
 		printf("Texto: %s\n", lpost.text);
@@ -95,21 +95,21 @@ void buscarUser(FILE* file){
 		printf("Likes: %d\n", lpost.like_count);
 		printf("Linguagem: %s\n", lpost.language);
 		printf("Compartilhamentos: %d\n", lpost.share_count);
-		printf("Views: %d\n", lpost.views_count);		
+		printf("Views: %d\n", lpost.views_count);
 	}
 	else
 	    printf("\nRegistro nao encontrado\n");
 }
-
+// 4)
 void buscarRRN(FILE* file){
 	post lpost;
 	int find=-1;
-	
+
 	printf("\n\nInsira o RRN para buscar\n");
 	scanf("%d", &find);
-	
+
 	fseek(file, find * sizeof(post), 0);
-	
+
 	if(fread(&lpost, sizeof(post), 1, file) > 0){
 	    printf("\n***** Registro %d *****\n", find);
 		printf("Texto: %s\n", lpost.text);
@@ -118,7 +118,7 @@ void buscarRRN(FILE* file){
 		printf("Likes: %d\n", lpost.like_count);
 		printf("Linguagem: %s\n", lpost.language);
 		printf("Compartilhamentos: %d\n", lpost.share_count);
-		printf("Views: %d\n", lpost.views_count);		
+		printf("Views: %d\n", lpost.views_count);
 	}
 	else
 	    printf("\nRegistro nao encontrado\n");
